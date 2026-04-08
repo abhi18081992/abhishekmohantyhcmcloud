8e7f3e9ded73a45e1c0941f496604e55c9f570c5
abhi18081992 <mohanty.abhishek51@gmail.com>
2026-04-08 15:04:25 +0530

    Remove sitemap integration to unblock build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.abhishekmohantyhcmcloud.com',
-  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
