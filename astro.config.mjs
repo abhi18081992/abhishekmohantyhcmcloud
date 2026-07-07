2b6614f42b9c35ae98ba3b052981f3483061bf24
Abhishek Mohanty <mohanty.abhishek51@gmail.com>
2026-07-08 01:04:00 +0530

    Switch to light code theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config'; 
-import rehypeRaw from 'rehype-raw'; 
  
 export default defineConfig({ 
   site: 'https://www.abhishekmohantyhcmcloud.com', 
   markdown: { 
     allowDangerousHtml: true, 
-    rehypePlugins: [rehypeRaw], 
-    shikiConfig: { theme: 'one-dark-pro', wrap: true }, 
+    shikiConfig: { theme: 'github-light', wrap: true }, 
   }, 
   build: { format: 'directory' }, 
 }); 
